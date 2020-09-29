f76aedac1155dd36ac43ef4cb878fc467bf5f1e1
coderrick <derrick.duncan@temple.edu>
2020-09-29 16:32:21 -0400

    Added route to test cookie-parser

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
+const cookieParser = require('cookie-parser');
 
 console.log("Go to localhost:3000");
+//Allow static files to be served
+//app.use(express.static('static'));
 
+app.use(cookieParser());
 app.get('/', function(req, res){
-    res.send("Hello world!");
+    res.send("Hello world!");  
  });
+
+ app.get('/cookietest', function(req, res){
+    res.cookie('test', 'cookie').send('cookie set');
+ })
  
  app.listen(3000);
