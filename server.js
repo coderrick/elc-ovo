eef35b5d25a0d4a05c70ff052c74f3c87e12edf9
coderrick <derrick.duncan@temple.edu>
2020-09-29 19:08:57 -0400

    Setup rendering for ejs templates

--- server.js
+++ server.js
@@ -3,16 +3,25 @@ const app = express()
 const cookieParser = require('cookie-parser');
 
 console.log("Go to localhost:3000");
+
 //Allow static files to be served
-//app.use(express.static('static'));
+app.use(express.static('static'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
 app.use(cookieParser());
-app.get('/', function(req, res){
-    res.send("Hello world!");  
- });
 
- app.get('/cookietest', function(req, res){
+app.get('/', function (req, res) {
+    res.render(__dirname + '/static/index');
+});
+
+app.get('/testingcenters', function (req, res) {
+    res.render(__dirname + '/static/testing')
+});
+
+app.get('/cookietest', function (req, res) {
     res.cookie('test', 'cookie').send('cookie set');
- })
- 
- app.listen(3000);
+})
+
+app.listen(3000);
